70e642492a71bd001ca3c6c05eef33dd8a0f477d
sudo-alex1990 <alexrfarnes@gmail.com>
2021-03-18 18:46:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Infinite Scroll
+
+> Implementation of the infinite scroll with the Fetch API and Unsplash API.
+
+## Table of contents
+
+- [General info](#general-info)
+- [Screenshots](#screenshots)
+- [Built With](#built-with)
+- [Features](#features)
+- [Status](#status)
+- [Inspiration](#inspiration)
+- [Contact](#contact)
+
+## General info
+
+Practice use of the Fetch API with async/await to retrieve random pictures from the Unsplash API. Also, practice the scroll and load event listener to estimate when to make the next call to the API for a smooth infinite scroll.
+
+## Screenshots
+
+![Screenshot]()
+
+[Demo]()
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- Requires an Unsplash API key
+
+## Features
+
+List of features ready and TODOs for future development
+
+- Fetch API
+- Unsplash API
+- Infinite Scroll
+
+## Status
+
+Project is: _finished_.
+
+## Inspiration
+
+Project based on [JavaScript Web Projects: 20 Projects to Build Your Portfolio](https://www.udemy.com/course/javascript-web-projects-to-build-your-portfolio-resume/)
+
+## Contact
+
+- GitHub [@AlexRFarnes](https://github.com/AlexRFarnes)
+- Twitter [@AlexRFarnes](https://twitter.com/alexrfarnes)
+- Linkedin [@AlexRFarnes](https://www.linkedin.com/in/alexrfarnes/)
---
+++ script.js
@@ -0,0 +1,72 @@
+// Unsplash API
+const count = 30;
+const apiKey = '';
+const API_Url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=landscape`;
+
+// https://api.unsplash.com/photos/random/?client_id=Q2IYI00-m9qT27QSlR7V_do1vJzYpuzJBUKIVoDZpQU&count=30&orientation=landscape
+
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let loadComplete = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++
+    console.log(imagesLoaded);
+    if(imagesLoaded === totalImages) {
+        loader.hidden = true;
+        loadComplete = true;
+        imagesLoaded = 0;
+    }
+}
+
+// Create the links and photos from the response and add to the DOM
+function displayPhotos() {
+    totalImages = photosArray.length;
+    // imagesLoaded = 0;
+    photosArray.forEach(photo => {
+        // Create an <a> to link to Unsplash;
+        const link = document.createElement('a');
+        link.href = photo.links.html;
+        link.target = '_blank';
+        // Create an <img> for photo;
+        const img = document.createElement('img');
+        img.src = photo.urls.regular;
+        img.alt = photo.alt_description;
+        img.title = photo.alt_description;
+
+        // Event listener to check when each image has loaded
+        img.addEventListener('load', imageLoaded)
+        // Append the img to the link
+        link.appendChild(img);
+        imageContainer.appendChild(link);
+    });
+}
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(API_Url);
+        photosArray  = await response.json();
+        displayPhotos();
+        
+    } catch (error) {
+        console.log('Ooops! Sorry, something must have gone wrong')
+    }
+}
+
+//Check if scrolling near the bottom of the page, and load more photos
+window.addEventListener('scroll', (event) => {
+    if(window.scrollY + window.innerHeight >= document.body.offsetHeight - 1000 && loadComplete) {
+        loadComplete = false;
+        getPhotos();
+    }
+})
+
+
+// On Load
+getPhotos();
